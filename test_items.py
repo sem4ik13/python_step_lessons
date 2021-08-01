272a1c9de23b84d2e09ac6c6c1a666e80addb359
sem4ik13 <51079351+sem4ik13@users.noreply.github.com>
2021-08-01 15:04:21 +0500

    update

--- test_items.py
+++ test_items.py
@@ -4,5 +4,6 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 def test_button(browser):
 	browser.get(link)
-	browser.find_element_by_css_selector(".btn.btn-lg.btn-primary.btn-add-to-basket")
-	time.sleep(1)
+	button = browser.find_element_by_css_selector(".btn.btn-lg.btn-primary.btn-add-to-basket")
+	assert (button.is_enabled()), f"Button is not clickable"
+	time.sleep(3)
