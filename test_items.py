8124a79139cf8dd1984b86d6eb627aa0db069eec
sem4ik13 <51079351+sem4ik13@users.noreply.github.com>
2021-08-01 14:44:14 +0500

    update

--- test_items.py
+++ test_items.py
@@ -1,5 +1,5 @@
-link = "http://selenium1py.pythonanywhere.com/"
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
-def test_guest_should_see_login_link(browser):
+def test_button(browser):
 	browser.get(link)
-	browser.find_element_by_css_selector("#login_link")
+	browser.find_element_by_css_selector(".btn.btn-lg.btn-primary.btn-add-to-basket")
